650e87763422dffc0baa7033e3a6f99a876f88ae
dennymario04 <denny.mario.business@gmail.com>
2022-11-28 12:52:59 +0700

    Mengenal fungsi self

--- oop.py
+++ oop.py
@@ -1,18 +1,30 @@
 # buat class mobile legend
 
-class MobileLegend :
+class GameMobile :
 #Atribut
     Name = "Mobile Legend Bang Bang"
     Genre = "Moba"
-
-
-
+#Method
+    def change_name(self,new_name):
+        self.Name = new_name
+    def change_genre(self,new_genre):
+        self.Genre = new_genre
 #instansiasi
-game = MobileLegend()
+game = GameMobile()
 """
-Instansiasi berguna untuk akases atribut didalam sebuah class
+Instansiasi berguna untuk akses atribut didalam sebuah class
 
 """
 print(game.Name)
 print(game.Genre)
+
+game.change_name("Clash Of Clans")
+game.change_genre("Strategy")
+
+print(game.Name)
+print(game.Genre)
+
+"""
+Self berfungsi sebagai penunjuk objek / method itu sendiri
+"""
 
