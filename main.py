2a7b41233c333060575137e0bfa7c5002f66482e
dennymario04 <denny.mario.business@gmail.com>
2022-11-28 11:45:46 +0700

    New learning curve

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Python input and output
+
+"""
+
+# Output exam
+
+print("Hello World!")
+
+# Input 
+
+#beautiful_number = input("Input Beautiful number ")
+
+"""
+More about input
+if you want to prin string such as letter , punctuation marks number and letter
+N number of times, The asterisk (*) will perform repetitions
+"""
+
+print("5"*6)
---
+++ oop.py
@@ -0,0 +1,18 @@
+# buat class mobile legend
+
+class MobileLegend :
+#Atribut
+    Name = "Mobile Legend Bang Bang"
+    Genre = "Moba"
+
+
+
+#instansiasi
+game = MobileLegend()
+"""
+Instansiasi berguna untuk akases atribut didalam sebuah class
+
+"""
+print(game.Name)
+print(game.Genre)
+
