e29340c4b936e289e92ff2e5f493998ec6ba188f
dennymario04 <denny.mario.business@gmail.com>
2022-11-28 12:52:28 +0700

    Mengenal tipe data di python

---
+++ data.py
@@ -0,0 +1,17 @@
+# Dictionary
+
+Mahasiswa = {
+    'Nama' : 'Denny Mario',
+    'Alamat' : 'Binjai Timur',
+    'Makanan': 'Sate Padang'
+}
+
+# List
+
+Kampus = [ # semua list dimulai index 0
+    "USU", # 0
+    "UNPAB", # 1
+    "UINSU" # 2
+]
+
+print(Kampus[0])
