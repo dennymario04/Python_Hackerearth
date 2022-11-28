cb846d3862fd5cbdc644d0cf3c9a64aeef9010bb
dennymario04 <denny.mario.business@gmail.com>
2022-11-28 12:52:43 +0700

    Mengenal fungsi di python

---
+++ function.py
@@ -0,0 +1,11 @@
+def pertambahan(num1,num2):
+    hasil = num1 + num2
+    return hasil
+def namaSaya():
+    print("Nama saya adalah Denny Mario")
+
+
+
+Tambahan = pertambahan(1,2)
+print(Tambahan)
+namaSaya()
